b4653ec8cac50ff0a45924ddafc382c1af9bb350
ntibbals <ntibbals@outlook.com>
2019-01-03 17:28:11 -0800

    completed array max

--- Pre-CodeChallenges/Program.cs
+++ Pre-CodeChallenges/Program.cs
@@ -20,7 +20,7 @@ namespace Pre_CodeChallenges
             string result = Console.ReadLine();
             if (result == "1")
             {
-                
+                ArrayMax();
                 return true;
             }
             else if (result == "2")
@@ -41,6 +41,34 @@ namespace Pre_CodeChallenges
             {
                 return true;
             }
+        }
+        private static void ArrayMax()
+        {
+            Console.Clear();
+            Console.WriteLine("Challenge 1: Array Max Result");
+
+            int[] randomArray = new int[5];
+            Random myRandom = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                randomArray[i] = myRandom.Next(1, 11);
+            }
+            int score = 0;
+
+
+            Console.WriteLine("Select one of the following numbers: ");
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}", randomArray[0], randomArray[1], randomArray[2], randomArray[3], randomArray[4]);
+            string result = Console.ReadLine();
+            bool userInput = Int32.TryParse(result, out int number);
+            for (int i = 0; i < randomArray.Length; i++)
+            {
+                if (number == randomArray[i])
+                    score += randomArray[i];
+
+            }
+            Console.WriteLine("Your score is {0}!", score);
+            Console.ReadLine();
+
         }
     }
 }
--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 Instructions:  Given an array of numbers, allow the end user the ability to choose from the array one number that exists. Then output a score based on the number selected.
 
 Resources:
+
+https://stackoverflow.com/questions/24443827/reading-an-integer-from-user-input
+
+https://docs.microsoft.com/en-us/dotnet/api/system.int32.tryparse?view=netframework-4.7.2
+
+##SUCCESSFUL TEST BELOW
+
+![SCREENSHOT](https://github.com/ntibbals/Prework-CodeChallenges/blob/master/Challenge1-Screenshot.PNG)
