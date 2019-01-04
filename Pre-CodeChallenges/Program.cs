769a3ff89b726fce8544b004365bbde3431da13b
ntibbals <ntibbals@outlook.com>
2019-01-03 17:06:18 -0800

    updated readme and  started challenge 1

--- Pre-CodeChallenges/Program.cs
+++ Pre-CodeChallenges/Program.cs
@@ -6,7 +6,41 @@ namespace Pre_CodeChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                displayMenu = MainMenu();
+            }
+        }
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option: ");
+            Console.WriteLine("1) Array Max Result");
+            string result = Console.ReadLine();
+            if (result == "1")
+            {
+                
+                return true;
+            }
+            else if (result == "2")
+            {
+               
+                return true;
+            }
+            else if (result == "3")
+            {
+              
+                return true;
+            }
+            else if (result == "4")
+            {
+                return true;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Prework-Challenges
+
+###Problem 1: Array Max Result
+
+Instructions:  Given an array of numbers, allow the end user the ability to choose from the array one number that exists. Then output a score based on the number selected.
+
+Resources:
