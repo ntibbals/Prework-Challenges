38552ca9bac39f43a75722b421d55f266a60cbf8
ntibbals <ntibbals@outlook.com>
2019-01-03 16:58:35 -0800

    project initialized

---
+++ Pre-CodeChallenges/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pre_CodeChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
